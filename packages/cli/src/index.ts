c4dcf1b2919e8f728124f0606847c1a01bf50181
Alexander Snov <snov.contact@gmail.com>
2019-09-03 15:39:51 +0700

    Add version info

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,13 +1,16 @@
+const version = () => require("./version").version;
+
 const getProgram = (query: string) => require(query);
 
 const run = (args: string[]) => {
-  console.log(`Denyok cli is running`, args);
+  console.log(`Denyok cli-${version()} is running`, args);
+  console.log("cwd", process.cwd());
+
   const programToRunQuery = args[2];
   try {
     const { version, description, main } = getProgram(programToRunQuery);
 
     console.log(description(), version());
-    console.log('cwd', process.cwd());
 
     main(args);
   } catch (e) {
