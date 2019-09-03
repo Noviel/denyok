c6f77a61df51789ccc72d07aa12e4d97b02d92dd
Alexander Snov <snov.contact@gmail.com>
2019-09-03 23:43:57 +0700

    Add scaffolding project from git feature

--- packages/task-new-project/src/index.ts
+++ packages/task-new-project/src/index.ts
@@ -1,5 +1,6 @@
 import * as fs from "fs";
 import { createFile } from "@denyok/effect-fs";
+import { fromGit } from "@denyok/effect-git";
 
 export const version = () => require("../package").version as string;
 
@@ -28,12 +29,48 @@ interface NewProjectOptions {
 
 interface RunOptions {
   rootPath: string;
+  from: string;
+}
+
+interface NewProjectGenerator {
+  work(options: NewProjectOptions & RunOptions);
 }
 
 const mapOptionsToPackageJsonLike = (options: NewProjectOptions) => {
   return {
     name: "unknown",
-    description: options.description,
+    description: options.description
+  };
+};
+
+const standardGenerator = (): NewProjectGenerator => {
+  return {
+    async work(args) {
+      const packageJsonContent = mapOptionsToPackageJsonLike(args);
+
+      const rootPath = args.rootPath;
+
+      if (rootPath) {
+        await fs.mkdirSync(rootPath);
+      }
+
+      console.log(`1. bootstrap folder structure`, args.description);
+      console.log(
+        `Creating file...`,
+        createFile(
+          `${rootPath}/package.json`,
+          JSON.stringify(packageJsonContent, undefined, 2)
+        )
+      );
+
+      console.log(
+        `Creating denyok.json file...`,
+        createFile(
+          `${rootPath}/denoyk.json`,
+          JSON.stringify(args, undefined, 2)
+        )
+      );
+    }
   };
 };
 
@@ -41,30 +78,17 @@ export const main = async (args: NewProjectOptions & RunOptions) => {
   console.log(`Creating new project`);
   console.log(`Args:\n`, args);
 
-  const packageJsonContent = mapOptionsToPackageJsonLike(args);
+  const rootPath = args.rootPath || "";
 
-  const rootPath = args.rootPath || '';
-
-  if (rootPath) {
-    await fs.mkdirSync(rootPath);
+  if (args.from) {
+    if (args.from.startsWith("git:")) {
+      fromGit(args.from.substring("git:".length), rootPath);
+    }
+  } else {
+    // use standard generator
+    const gen = standardGenerator();
+    await gen.work({ ...args, rootPath });
   }
-
-  console.log(`1. bootstrap folder structure`, args.description);
-  console.log(
-    `Creating file...`,
-    createFile(
-      `${rootPath}/package.json`,
-      JSON.stringify(packageJsonContent, undefined, 2)
-    )
-  );
-
-  console.log(
-    `Creating denyok.json file...`,
-    createFile(
-      `${rootPath}/denoyk.json`,
-      JSON.stringify(args, undefined, 2)
-    )
-  );
 
   return args;
 };
