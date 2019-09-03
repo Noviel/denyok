971abfd8dbb9380b9e6851af6e1b59f0353dda55
Alexander Snov <snov.contact@gmail.com>
2019-09-04 02:03:07 +0700

    Fix tsconfig.json filename

--- packages/task-new-package/src/core.ts
+++ packages/task-new-package/src/core.ts
@@ -64,7 +64,7 @@ export const main = async (args: Options) => {
   );
   if (args.typescript) {
     createFile(
-      `${thisPackagePath}/typescript.json`,
+      `${thisPackagePath}/tsconfig.json`,
       typescriptConfigContent(packagesPathToLevel(packagesPath))
     );
   }
